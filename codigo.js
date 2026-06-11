4463c8f2bb5e7a96f5426d116b26f2f8c455d630
Claudio-1201 <74226746@continental.edu.pe>
2026-06-11 18:03:36 -0500

    newww
    Signed-off-by: Claudio-1201 <74226746@continental.edu.pe>

---
+++ codigo.js
@@ -0,0 +1,13 @@
+const contenedor = document.querySelector(".contenedor");
+
+function irDerecha(){
+    contenedor.style.transform = "translateX(-100vw)";
+}
+
+function irIzquierda(){
+    contenedor.style.transform = "translateX(0)";
+}
+
+function mostrarMensaje(){
+    alert("GRACIAS POR SER UNA BUENA AMIGA NALGAA");
+}
